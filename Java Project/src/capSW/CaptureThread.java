ed14c59aba13836b70831e7b3624f5dce2cfb17f
Lewis <lewis.turner.2016@gmail.com>
2026-04-29 18:57:14 +0100

    rest of collecting shared frame data

--- Java Project/src/capSW/CaptureThread.java
+++ Java Project/src/capSW/CaptureThread.java
@@ -17,60 +17,16 @@ import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
 
 public class CaptureThread extends Thread {
-//	private ResourceSync<Integer> widthSW, heightSW;
-	
-	private Run main;
 	private FrameData frameData;
 	private ResourceSync<Boolean> running;
-//	private ResourceSync<BufferedImage> img;
-//	private String imgEncoded;
 	
-	public CaptureThread(Run main, FrameData frameData) {
-		this.main = main;
+	public CaptureThread(FrameData frameData) {
 		this.frameData = frameData;
 		
 		running = new ResourceSync<>(true);
 		
 		this.frameData.updateResolution(288, 160);
-//		widthSW = new ResourceSync<>(288);
-//		heightSW = new ResourceSync<>(160);
-		
-//		img = new ResourceSync<>(new BufferedImage(widthSW.get(), heightSW.get(), BufferedImage.TYPE_INT_RGB));
-//		imgEncoded = "";
 	}
-	
-	// Thread-safe change width/height vars
-//	public void setOutputRes(int w, int h) {
-//		widthSW.set(w);
-//		heightSW.set(h);
-//	}
-	
-//	private void encode() {
-//		StringBuilder packet = new StringBuilder();
-//		int width = img.get().getWidth();
-//		int height = img.get().getHeight();
-//		
-//		for (int y = 0; y < height; y++) {
-//			for (int x = 0; x < width; x++) {
-//				int color = img.get().getRGB(x, y);
-//				int startX = x;
-//				
-//				// Find how many horizontal pixels have the same color
-//				while (x + 1 < width && img.get().getRGB(x + 1, y) == color) {x++;}
-//				int runWidth = (x - startX) + 1;
-//				
-//				// Format: R,G,B,X,Y,W| (Quantized to 0-255)
-//				Color c = new Color(color);
-//				packet.append(String.format("%d,%d,%d,%d,%d,%d|", 
-//				c.getRed(), c.getGreen(), c.getBlue(), startX, y, runWidth));
-//			}
-//		}
-//		imgEncoded = packet.toString();
-//	}
-	
-//	public BufferedImage getImage() {
-//		return img.get();
-//	}
 	
 	public void startCapture() {
 		running.set(true);
@@ -86,18 +42,7 @@ public class CaptureThread extends Thread {
 			frame = grabber.grab();
 			if (frame != null) {
 				BufferedImage screen = converter.getBufferedImage(frame); // get frame as BufferedImage
-	            
-	            // Resize image to SW resolution
-//	            Image scaled = screen.getScaledInstance(widthSW.get(), heightSW.get(), Image.SCALE_FAST);
-//	            img.set(new BufferedImage(widthSW.get(), heightSW.get(), BufferedImage.TYPE_INT_RGB));
-//	            img.get().getGraphics().drawImage(scaled, 0, 0, null);
-//				
-//				encode(); //convert image buffer to string
-//				
-//				main.setFrame(img.get());
-//				main.updateFrame(imgEncoded); // send frame to main
-				
-				frameData.updateImg(screen);
+	            frameData.updateImg(screen);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -107,7 +52,7 @@ public class CaptureThread extends Thread {
 	@Override
 	public void run() {
 		org.bytedeco.javacv.FFmpegLogCallback.set(); // enable better FFmpeg logs
-		String filter = "ddagrab=framerate=1:draw_mouse=0,hwdownload,format=bgra"; // different filters separated by "," | different settings of same filter separated by ":"
+		String filter = "ddagrab=framerate=2:draw_mouse=0,hwdownload,format=bgra"; // different filters separated by "," | different settings of same filter separated by ":"
 		
 		// run inside try-with-resources to prevent resource leaks
 		try (
